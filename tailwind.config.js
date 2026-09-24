6a1b6cc0e82eb89f96b0c47dfc78175ad9488fda
0zbimstudio-ui <0zbimstudio@gmail.com>
2026-09-24 12:07:33 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        bgDark: '#0B0D10',
+        cardDark: '#141821',
+        cardHover: '#1D2330',
+        cyanAccent: '#00F0FF',
+        copperAccent: '#E07A5F',
+      },
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+        heading: ['Space Grotesk', 'sans-serif'],
+        mono: ['JetBrains Mono', 'monospace'],
+      },
+    },
+  },
+  plugins: [],
+}
